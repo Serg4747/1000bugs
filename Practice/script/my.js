df811de2b461715782d9916368f1b2578ff8dd22
serg4747 <neftekum.serg@mail.ru>
2019-05-24 10:51:17 +0300

    раскидал по папкам

--- Practice/script/my.js
+++ Practice/script/my.js
@@ -1,31 +1,31 @@
 let deadline = '2019-06-01';
 
 function getTimeRemainig(endtime) {
-  let t = Date.parse(endtime) - Date.parse(new Date()),
-  seconds = Math.floor((t/1000) % 60),
-  minutes = Math.floor((t/1000/60) % 60),
-  hours = Math.floor((t/1000/60/60) % 24),
-  days = Math.floor(t/(1000*60*60*24));
-  return{
-    'total' : t,
-    'days' : days,
-    'hours' : hours,
-    'minutes' : minutes,
-    'seconds' : seconds
-  };
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/1000/60/60) % 24),
+        days = Math.floor(t/(1000*60*60*24));
+    return {
+        total : t,
+        days : days,
+        hours : hours,
+        minutes : minutes,
+        seconds : seconds,
+    };
 }
 
 function setClock(id, endtime) {
-  let timer = document.getElementById(id),
-      days = timer.querySelector('.days'),
-      hours = timer.querySelector('.hours'),
-      minutes = timer.querySelector('.minutes'),
-      seconds = timer.querySelector('.seconds'),
-      timeInterval = setInterval(updateClock,1000);
-      if (t.total <= 0) {
+    let timer = document.getElementById(id),
+        days = timer.querySelector('.days'),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock,1000);
+    if(t.total <= 0) {
         clearInterval(timeInterval);
-      }
-      function updateClock() {
+    }
+    function updateClock() {
         let t = getTimeRemainig(endtime);
         if(t.days < 10 ){days.textContent = '0'+ t.days;}
         else{days.textContent = t.days;}
@@ -38,7 +38,7 @@ function setClock(id, endtime) {
         // hours.textContent = t.hours;
         // minutes.textContent = t.minutes;
         // seconds.textContent = t.seconds;
-      }
+    }
 
 }
 
