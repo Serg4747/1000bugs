71cf505a0446ec8fcb577e80e3aebf15887a0073
serg4747 <neftekum.serg@mail.ru>
2019-06-02 19:27:29 +0300

    рабочий вариант

--- Jquery/OWLCarousel/js/script.js
+++ Jquery/OWLCarousel/js/script.js
@@ -1,10 +1,29 @@
 $(function() {
-
+    var $items = $('.item');
     $('.faq').owlCarousel({
         loop: true,
         margin: 10,
         autoHeight: true,
         items: 5,
-
+        onInitialized: function() {
+            equalsHeight($items);
+        },
+        onResized: function() {
+            equalsHeight($items);
+        }
     });
+
+    function equalsHeight($items) {
+        var max = 0;
+        $items.each(function() {
+            var h = $(this).find('> div').outerHeight();
+            if(h > max) {
+                max = h;
+            }
+        });
+        $items.css('min-height', max + 'px');
+        setTimeout(function() {
+            $('.faq').trigger('refresh.owl.carousel');
+        }, 1);
+    }
 });
